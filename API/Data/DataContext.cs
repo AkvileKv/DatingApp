937407730a5244cb44ead760c632413d1f7bae39
AkvileKv <kvietkauskaite.akvile@gmail.com>
2021-01-27 17:08:39 +0200

    Add context class and database

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {   //dependency injection container:
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
