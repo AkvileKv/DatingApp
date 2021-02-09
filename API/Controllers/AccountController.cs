f500925858b2d89f7e4548a28e773e27c8d1a6d0
AkvileKv <>
2021-02-09 18:13:48 +0200

    Add login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,6 +38,27 @@ namespace API.Controllers
             return user;
         } 
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+        {
+            var user = await __context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+             using var hmac = new HMACSHA512(user.PasswordSalt);
+
+             var ComputedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+             for (int i = 0; i < ComputedHash.Length; i++)
+             {
+                 if (ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+             }
+
+             return user;
+
+        }
+
+
         private async Task<bool>UserExist(string username)
         {
             return await __context.Users.AnyAsync(x => x.UserName == username.ToLower());
