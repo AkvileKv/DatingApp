39fca83aab88359ea564e61f68febf7a89d37da4
AkvileKv <>
2021-02-15 19:54:37 +0200

    Add user observable

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,11 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({ //this service can be injected into other components or other services in our application
   providedIn: 'root'
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   //inject the HTTP client into our account service
   constructor(private http: HttpClient) { 
@@ -13,6 +18,23 @@ export class AccountService {
   }
   //receive the credentials from the login form from navbar
   login(model: any){
-      return this.http.post(this.baseUrl + 'account/login', model);
+      return this.http.post(this.baseUrl + 'account/login', model).pipe(
+        map((response: User) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+        })
+      );
+  }
+
+  setCurrentUser(user:User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
