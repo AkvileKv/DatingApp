6e80086e68b76128bf97598abc640d9ddc887a1d
AkvileKv <>
2021-02-25 20:31:52 +0200

    Add routes, toast service and route guard

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
   //to store users input:
   model: any = {};
   //public - to access a service inside out templates
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService ) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +24,8 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);    
+      console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
