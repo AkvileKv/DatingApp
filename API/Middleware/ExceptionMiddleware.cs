1d1f554930642b82ad0d8e6a92dbc17dc6222d22
AkvileKv <>
2021-03-08 15:19:41 +0200

    Add Exception middleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
@@ -10,7 +11,47 @@ namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
+
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _env;
+
+        //request delegates, what's coming up next in the middlewhere pipeline
+        //ILogger - to log out exceptions into the terminal
+        //to check what environment we're running in, production / development
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _next = next;
+            _logger = logger;
+
+        }
+
+        //to give this middleware its required method
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try 
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, "Internal server Error");
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+
+            }
+
+        }
         
-        
-    }
+}
 }
