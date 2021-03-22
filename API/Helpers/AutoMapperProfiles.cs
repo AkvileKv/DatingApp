c9a0a33ae803dc8a75df1f407b149190b1ec4e34
AkvileKv <kvietkauskaite.akvile@gmail.com>
2021-03-22 19:22:03 +0200

    Add DTO for Members and AutoMapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        { //where to map from, and where to map to
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src 
+            => src.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
